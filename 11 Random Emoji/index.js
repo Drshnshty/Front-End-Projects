2d56d5fcc2d7535b5bd6e7df854e9b63195a5f2a
DarshanBShety <drsnshty@gmail.com>
2025-04-23 16:28:47 +0530

    Random emoji generator

---
+++ 11 Random Emoji/index.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector("#emoji");
+const emojis=["🎯", "🌈", "💻", "🚀", "🔥", "🎉", "🍕", "📚", "🧘‍♂️", "💡","🎨", "🌟", "🚴‍♂️", "🎧", "🍔", "🏖️", "🤖", "💪", "🎮", "🍩","🍏", "🍎", "🍌", "🍒", "🍉"];
+btn.addEventListener('mouseover',()=>{
+  btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+});
