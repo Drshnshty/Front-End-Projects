490a8c7254ac535ee6df3f9bbffcf2b9d9d0f361
DarshanBShety <drsnshty@gmail.com>
2025-04-24 15:42:00 +0530

    FeedBack

---
+++ 13 Feedback/index.js
@@ -0,0 +1,26 @@
+const ratings=document.querySelectorAll(".rating");
+const ratingsContainer=document.querySelector(".ratings-conatiner");
+const sendBtn=document.querySelector("#send");
+const panel=document.querySelector("#panel");
+
+let selectedRating="Satisfied";
+//Attach Events
+ratingsContainer.addEventListener("click", e => {
+  const rating = e.target.closest('.rating'); // Finds the nearest .rating parent
+  if (rating) {
+    removeActive();
+    rating.classList.add("bg-gray-600", "rounded", "transition-all", "duration-1000");
+    selectedRating = rating.querySelector("small").innerHTML;
+  }
+});
+
+sendBtn.addEventListener("click",()=>{
+  panel.innerHTML=`<p class="text-6xl mb-3">🧡</p> <strong class="text-4xl">Thank You</strong> <br> <strong class="text-2xl">FeedBack-${selectedRating}</strong>`
+
+})
+
+function removeActive(){
+  for(let i=0;i<ratings.length;i++){
+    ratings[i].classList.remove("bg-gray-600", "rounded", "transition-all", "duration-1000");
+  }
+}
