bf9b26be0f63ff8edb60cfc52c68027850b5c09e
DarshanBShety <drsnshty@gmail.com>
2025-05-23 00:49:45 +0530

    page Scroll Indicator

--- 13 Feedback/index.js
+++ 13 Feedback/index.js
@@ -6,16 +6,18 @@ const panel=document.querySelector("#panel");
 let selectedRating="Satisfied";
 //Attach Events
 ratingsContainer.addEventListener("click", e => {
-  const rating = e.target.closest('.rating'); // Finds the nearest .rating parent
+  const rating = e.target.closest('.rating'); 
+  //const rating=e.target.parenetNode.classList,contains(".rating");
   if (rating) {
     removeActive();
     rating.classList.add("bg-gray-600", "rounded", "transition-all", "duration-1000");
     selectedRating = rating.querySelector("small").innerHTML;
+    // selectedrating=e.target.nextElementSibling.innerHTML;
   }
 });
 
 sendBtn.addEventListener("click",()=>{
-  panel.innerHTML=`<p class="text-6xl mb-3">🧡</p> <strong class="text-4xl">Thank You</strong> <br> <strong class="text-2xl">FeedBack-${selectedRating}</strong>`
+  panel.innerHTML=`<p class="text-6xl mb-3">🧡</p> <strong class="text-4xl">Thank You</strong> <br> <strong class"text-2xl">FeedBack-${selectedRating}</strong>`
 
 })
 
